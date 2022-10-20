f6fa8adf209199270ce4c0f292a1073b4cca5a8f
Magda-GitHub <antoniakmagdalena@wp.pl>
2022-10-20 14:02:25 +0200

    js date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavLink =document.querySelectorAll('.nav__link')
+// const navBtnBars = document.querySelector('.burger-btn__bars')
+// const sectionAll = document.querySelectorAll('.section')
+// const sectionHotel = document.querySelector('.section__hotel')
+const footerYear = document.querySelector('.footer__year')
 
 const navigation = () => {
     nav.classList.toggle('nav--activity')
@@ -13,19 +17,30 @@ const navigation = () => {
 
 }
 
-const burgerBtn = () => {
-    const currentSection = window.scrollY;
 
-    allSections.forEach(section => {
+// const handleSection = () => {
+//     const currentSection = window.scrollY;
 
-        if (section.classList.contains('light__section') && section.offsetTop <= currentSection + 60) {
-            navBtnBars.classList.add('black__bars')
-        } else if (!section.classList.contains('light__section') && section.offsetTop <= currentSection + 60) {
-            navBtnBars.classList.remove('black__bars')
-        }
-    })
+//     sectionAll.forEach(section => {
+
+//         if (section.classList.contains('light__section') && section.offsetTop <= currentSection + 5) {
+//             navBtnBars.classList.add('black__bars__color')
+//         } else if (!section.classList.contains('light__section') && section.offsetTop <= currentSection + 5){
+//             navBtnBars.classList.remove('black-bars-color')
+//         }
+//     })
+// }
+
+
+
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
 }
 
 
+
+handleCurrentYear();
 navBtn.addEventListener('click', navigation);
-window.addEventListener('scroll', burgerBtn)
+// window.addEventListener('scroll', handleSection);
